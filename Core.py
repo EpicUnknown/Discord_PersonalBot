c26528b292835630f0910333308d3751fc3e9c89
_iEpic <NoEmail>
2017-08-13 14:39:25 -0400

    Version 0.1-R1.10 + Added 'authorized_admin_roles' and 'authorized_mod_roles' + Added 'save_settings()' during logout

--- Core.py
+++ Core.py
@@ -14,6 +14,11 @@ custom_settings_filepath = 'C:\\Users\\David\\Desktop\\Settings.json'
 
 default_settings_filepath = 'Settings.json'
 settings_filepath = input('Where can we find bot settings? (Push Enter for default)> ')
+bot_version = '0.1-R1.10'
+online_users = []
+authorized_admin_roles = ['Admin']
+authorized_mod_roles = ['Moderator']
+authorized_users = []
 
 if settings_filepath == '':
     settings_filepath = default_settings_filepath
@@ -26,13 +31,15 @@ if my_file.is_file():
     file = open(settings_filepath, 'r')
     settings = json.load(file)
     
-    bot_version = settings['bot_version']
+    if settings['bot_version'] is bot_version:
+        bot_version = settings['bot_version']
     server_id = settings['server_id']
     owner_id = settings['owner_id']
     bot_token = settings['bot_token']
     bot_description = settings['bot_description']
     bot_prefix = settings['bot_prefix']
     authorized_users = settings['authorized_users']
+    
 else:
     print('Settings File does not exist, please follow along for setup.')
     server_id = input('What is the Server ID?> ')
@@ -40,24 +47,17 @@ else:
     bot_token = input('What is the Bots Token?> ')
     bot_description = input('What is the Bots Description?> ')
     bot_prefix = input('What is the Bots Command Prefix? (Default:~)> ')
-    bot_version = '0.1-R1.5'
     if bot_prefix == '':
         bot_prefix = '~'
     
 
 bot = commands.Bot(command_prefix=bot_prefix, description=bot_description + ' - Version: %s' % bot_version)
-online_users = []
-authorized_admin_roles = ['Admin']
-authorized_mod_roles = ['Moderator']
-authorized_users = []
 
 def set_globals():
     global bot_owner
     global bot_server
-    global debug_toggle_code
     bot_owner = bot.get_server(server_id).get_member(owner_id)
     bot_server = bot.get_server(server_id)
-    debug_toggle_code = 0
     
     auth_owner = 0
     for auth_user in authorized_users:
@@ -68,15 +68,14 @@ def set_globals():
 
 def save_settings():
     file = open(settings_filepath, 'w')
-    settings = {"bot_version" : bot_version, "server_id" : server_id, "owner_id" : owner_id, "bot_token" : bot_token, "bot_description" : bot_description, "bot_prefix" : bot_prefix, "authorized_users" : authorized_users}
+    settings = {"bot_version" : bot_version, "server_id" : server_id, "owner_id" : owner_id, "bot_token" : bot_token, "bot_description" : bot_description, "bot_prefix" : bot_prefix, "authorized_users" : authorized_users, "authorized_admin_roles" : authorized_admin_roles, "authorized_mod_roles" : authorized_mod_roles}
     json.dump(settings, file, indent=4, sort_keys=True)
     file.close()
     print('Settings Saved!')
 
 # Begin loading bot
 def debug(message):
-    if debug_toggle_code == 1:
-        print('[DEBUG] %s' % message)
+    print('')
 
 def refresh_online_users():
     online_users.clear()
@@ -114,7 +113,6 @@ def remove_authorized_user(user):
     member = bot_server.get_member_named(user)
     
     if member == None:
-        debug('[WARNING]: Member does not exist or is offline')
         return '[WARNING]: Member does not exist or is offline'
     else:
         user_found = 0
@@ -124,9 +122,7 @@ def remove_authorized_user(user):
         if user_found == 1:
             authorized_users.remove(member.name)
             return '%s removed successfully' % member.name
-            debug('%s Removed' % member.name)
         user_found = 0
-        debug('[WARNING]: %s is not an authorized user.' % member.name)
 
 #------------------------------------------------------------------------------
 
@@ -154,27 +150,27 @@ async def get_online_users():
 async def add_auth_user(context, msg):
     if check_authorized_user(context.message.author):
         await bot.send_message(context.message.author, add_authorized_user(msg))
-        debug(authorized_users)
 
 @bot.command(pass_context=True)
 async def remove_auth_user(context, msg):
     if check_authorized_user(context.message.author):
         await bot.send_message(context.message.author, remove_authorized_user(msg))
-        debug(authorized_users)
 
 @bot.command(pass_context=True)
-async def debug_toggle(context):
-    if debug_toggle_code == 0:
+async def debug_toggle(context, msg):
+    if msg == 'on':
         debug_toggle_code = 1
         await bot.send_message(context.message.author, 'Debug enabled. Check console for debug messages.')
-    else:
+        debug('Test')
+    elif msg == 'off':
         debug_toggle_code = 0
         await bot.send_message(context.message.author, 'Debug disabled.')
     
 @bot.command(pass_context=True)
 async def logout(context):
     if check_authorized_user(context.message.author):
-        await bot.say('Logging out...')
+        await bot.say('Saving and logging out...')
+        save_settings()
         await bot.logout()
         print('Bot Logged Out.')
   
